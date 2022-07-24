40955413bed061acca9230fc74b29026130658d3
zLuskx <matheusleidow@gmail.com>
2022-07-24 20:14:49 -0300

    estrutura do projeto

---
+++ src/server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+// definindo o template engine
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+//definindo os arquivos públicos
+app.use(express.static(path.join(__dirname, 'public')))
+
+//habilita o server para receber dados via post de formulários
+app.use(express.urlencoded({ extended: true }))
+
+// rotas
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Titulo Teste'
+    })
+})
+
+// erro 404
+app.use((req, res) => { // middleware
+    res.send('Página não encontrada!')
+})
+
+const port = process.env.PORT || 8080
+
+app.listen(port, () => {
+    console.log('Funcionando!')
+})
