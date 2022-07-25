606655fc872cefb926c1756e3766f8ef9456b6fb
zLuskx <matheusleidow@gmail.com>
2022-07-25 19:16:07 -0300

    adicionando registro de informações

---
+++ src/controllers/customers.js
@@ -0,0 +1,20 @@
+const CustomersModel = require('../models/customers')
+
+
+function add(req, res) {
+    const { name, age, email, password } = req.body
+
+    const register = new CustomersModel({
+        name,
+        age,
+        email,
+        password
+    })
+
+    register.save()
+    res.redirect('/register')
+}
+
+module.exports = {
+    add
+}
